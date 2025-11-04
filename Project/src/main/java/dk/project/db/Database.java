68ba33dff96e385b4d2f020b6cf042d84c75d1c4
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-11-04 12:09:05 +0100

    First Commit

--- Project/src/main/java/dk/project/db/Database.java
+++ Project/src/main/java/dk/project/db/Database.java
@@ -18,7 +18,7 @@ import java.sql.SQLException;
 public class Database {
 
     // Attributes
-    private static String databaseName = "Project";
+    private static String databaseName = "fog";
     private static final String URL = "jdbc:postgresql://localhost:5432/%s";
     private static final String USER = "postgres";
     private static final String PASSWORD = "postgres";
