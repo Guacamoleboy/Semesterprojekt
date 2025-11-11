55958953a096efa24774461ee8d21ea83a7d3e4c
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-11-11 12:45:50 +0100

    Workflow test

--- Project/src/test/java/dk/project/mapper/UserMapperTest.java
+++ Project/src/test/java/dk/project/mapper/UserMapperTest.java
@@ -44,8 +44,6 @@ class UserMapperTest {
 
     // ______________________________________________________________________
 
-    // Test af workflow
-
     @Test
     void shouldInsertAndRetrieveUser() throws SQLException, DatabaseException {
 
