6ee8f628452b9007ba8b6b6db99a4b57859af9a7
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-11-12 13:43:57 +0100

    Main Update

--- Visuals/js/preloader.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("load", () => {
-    setTimeout(() => {
-        const loader = document.querySelector(".preloader");
-        if (loader) loader.style.display = "none";
-    }, 1000); // !! Must match with the style.css value under .preloader !!
-});
