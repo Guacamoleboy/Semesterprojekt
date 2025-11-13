33620bf9cfc67b844d2e7d7032abbd4ed6d161f0
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2025-11-13 16:00:26 +0100

    Fixes for #97 review

---
+++ Project/src/main/java/dk/project/controller/AdminMenu/ProductController.java
@@ -0,0 +1,54 @@
+package dk.project.controller.AdminMenu;
+
+import dk.project.entity.AdminMenu.Product;
+import dk.project.exception.DatabaseException;
+import dk.project.mapper.ProductMapper;
+import io.javalin.Javalin;
+import io.javalin.http.Context;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class ProductController {
+
+    ProductMapper productMapper = new ProductMapper();
+
+    public static void registerRoutes(Javalin app) {
+
+        ProductController controller = new ProductController();
+
+        app.post("/getProducts", controller::getAllProducts);
+        app.post("/searchProducts", controller::searchProducts);
+    }
+
+    // _______________________________________________
+
+    public void getAllProducts(Context ctx) throws DatabaseException {
+
+        ctx.json(productMapper.getProducts());
+
+    }
+
+    // _______________________________________________
+
+    public void searchProducts(Context ctx) throws DatabaseException {
+        Product search = ctx.bodyAsClass(Product.class);
+
+        List<Product> result = productMapper.getProducts().stream()
+
+                .filter(p -> search.getId() <= 0 || String.valueOf(p.getId()).contains(String.valueOf(search.getId())))
+
+
+                .filter(p -> search.getTitle() == null || search.getTitle().isEmpty()
+                        || p.getTitle().toLowerCase().contains(search.getTitle().toLowerCase()))
+
+
+                .filter(p -> search.getSize() == null || search.getSize().isEmpty()
+                        || p.getSize().toLowerCase().contains(search.getSize().toLowerCase()))
+
+                .collect(Collectors.toList());
+
+        ctx.json(result);
+    }
+
+}
