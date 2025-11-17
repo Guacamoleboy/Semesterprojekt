ec2bb249c508647e733f81a814b435cd8831ef3b
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2025-11-17 15:02:58 +0100

    Unsused code

--- Project/src/main/resources/static/js/admin-menu.js
+++ Project/src/main/resources/static/js/admin-menu.js
@@ -3,7 +3,6 @@ document.addEventListener('DOMContentLoaded', () => {
     // Attributes
     const buttons = document.querySelectorAll('.guac-btn-profile');
     const sections = document.querySelectorAll('.profile-actual');
-    const addBtn = document.getElementById("addBalanceBtn");
 
     // _____________________________________________________________________________________________
 
@@ -52,53 +51,6 @@ document.addEventListener('DOMContentLoaded', () => {
             buttons.forEach(b => b.classList.remove('active'));
             btn.classList.add('active');
         });
-    });
-
-    // ____________________________________________________________________________________________
-
-    addBtn.addEventListener("click", async () => {
-
-        // Gets our input first
-        const username = document.querySelector("#addMoneyAdmin input[placeholder='Brugernavn...']").value.trim();
-        const id = document.querySelector("#addMoneyAdmin input[placeholder='ID...']").value.trim();
-        const amount = document.querySelector("#addMoneyAdmin input[placeholder='Beløb...']").value.trim();
-
-        // Validation
-        if ((!username && !id) || (username && id)) {
-            showNotification("Udfyld venligst kun ét af felterne", "orange")
-            return;
-        }
-
-        // Checks if there's an amount entered
-        if (!amount) {
-            showNotification("Tilføj beløb", "orange")
-            return;
-        }
-
-        // Execute if pass
-        try {
-
-            const formData = new URLSearchParams();
-            formData.append("username", username);
-            formData.append("id", id);
-            formData.append("amount", amount);
-
-            const res = await fetch("/admin/add-balance", {
-                method: "POST",
-                body: formData
-            });
-
-            // Very important. Will NOT redirect without.
-            if (res.redirected) {
-                window.location.href = res.url;
-            } else {
-                const text = await res.text(); alert(text);
-            }
-
-        } catch (err) {
-            console.error(err);
-            showNotification("Forbindelsesfejl", "red")
-        }
     });
 
 });
