2927f2cb4d1e1452ff635982f692dad0e2d12d37
Rovelt123 <78000528+Rovelt123@users.noreply.github.com>
2025-11-19 12:33:22 +0100

    Fik chat til at skrive lidt flere materialer (Aner ikke hvordan man bygger et skur og hvilke materialer man skal bruge)

--- Project/src/main/resources/static/postgre/dummy.sql
+++ Project/src/main/resources/static/postgre/dummy.sql
@@ -52,41 +52,77 @@ INSERT INTO products (title, description, size, quantity, price) VALUES
 ('Hammer', 'Stålhammer', '35cm', 80, 15.00);
 
 INSERT INTO materials_category (name) VALUES
-('Træ & Tagplader'),
-('Beslag & Skruer');
+('Træ'),
+('Metal'),
+('Plast/Polycarbonat'),
+('Tagmateriale'),
+('Gulv'),
+('Vinduer'),
+('Døre'),
+('Skruer'),
+('Beslag');
+
 
 INSERT INTO materials (category_id, name, description, unit, length, width, height, price) VALUES
--- Træ & Tagplader
+-- Træ
+-- Spær, Rem og stolper
+
 (1, 'Brædt 360', '25x200 mm trykimp. understernbrædder til for & bag ende', 'stk', 360, 25, 200, 20.00),
 (1, 'Brædt 540', '25x200 mm trykimp. understernbrædder til siderne', 'stk', 540, 25, 200, 25.00),
-(1, 'Overstern 360', '25x125 mm trykimp. oversternbrædder til forenden', 'stk', 360, 25, 125, 15.00),
+(1, 'Overstern 360', '25x125 mm trykimp. oversternbrædder til forende', 'stk', 360, 25, 125, 15.00),
 (1, 'Overstern 540', '25x125 mm trykimp. oversternbrædder til siderne', 'stk', 540, 25, 125, 18.00),
 (1, 'Lægte 420', '38x73 mm ubh. til bagside af dør', 'stk', 420, 38, 73, 5.00),
+(1, 'Lægte 600', '38x73 mm ubh. til tagstøtte', 'stk', 600, 38, 73, 7.00),
 (1, 'Reglar 270', '45x95 mm ub. løsholter til skur gavle', 'stk', 270, 45, 95, 3.00),
 (1, 'Reglar 240', '45x95 mm ub. løsholter til skur sider', 'stk', 240, 45, 95, 2.50),
-(1, 'Spær 600', '45x195 mm spærtræ ubh. remme i sider, sadles ned i stolper', 'stk', 600, 45, 195, 10.00),
-(1, 'Spær 480', '45x195 mm spærtræ ubh. remme i sider, sadles ned i stolper (skur del, deles)', 'stk', 480, 45, 195, 8.00),
-(1, 'Spær 600', '45x195 mm spærtræ ubh. 15 stk spær, monteres på rem', 'stk', 600, 45, 195, 12.00),
-(1, 'Stolpe 300', '97x97 mm trykimp. Stolper nedgraves 90 cm i jord', 'stk', 300, 97, 97, 15.00),
-(1, 'Brædt 210', '19x100 mm trykimp. beklædning af skur 1 på 2', 'stk', 210, 19, 100, 1.50),
+(1, 'Spær 480', '45x195 mm ubh. spær til skur', 'stk', 480, 45, 195, 8.00),
+(1, 'Spær 600', '45x195 mm ubh. spær til skur', 'stk', 600, 45, 195, 10.00),
+(1, 'Spær 720', '45x195 mm ubh. spær til større skur', 'stk', 720, 45, 195, 12.00),
+(1, 'Stolpe 300', '97x97 mm trykimp. stolper nedgraves 90 cm i jord', 'stk', 300, 97, 97, 15.00),
+(1, 'Stolpe 400', '97x97 mm trykimp. stolper til højere skur', 'stk', 400, 97, 97, 20.00),
+(1, 'Brædt 210', '19x100 mm trykimp. beklædning af skur', 'stk', 210, 19, 100, 1.50),
 (1, 'Vandbrædt 540', '19x100 mm trykimp. på stern i sider', 'stk', 540, 19, 100, 3.00),
 (1, 'Vandbrædt 360', '19x100 mm trykimp. på stern i forende', 'stk', 360, 19, 100, 2.50),
-(1, 'Plastmo Ecolite 600', 'Blåtonet tagplade, monteres på spær', 'stk', 600, NULL, NULL, 50.00),
-(1, 'Plastmo Ecolite 360', 'Blåtonet tagplade, monteres på spær', 'stk', 360, NULL, NULL, 35.00),
--- Beslag & Skruer
-(2, 'Bundskruer 200', 'Plastmo, 3 pakke, til tagplader', 'pakke', 200, NULL, NULL, 0.50),
-(2, 'Hulbånd 1x20mm', '10 mtr., 2 rulle, til vindkryds på spær', 'rulle', 1000, 20, NULL, 2.50),
-(2, 'Universal skruer 190mm højre', '15 stk til montering af spær på rem', 'stk', 190, NULL, NULL, 1.50),
-(2, 'Universal skruer 190mm venstre', '15 stk til montering af spær på rem', 'stk', 190, NULL, NULL, 1.50),
-(2, 'Skruer 4,5x60mm 200stk', '1 pakke til montering af stern & vandbrædt', 'pakke', 60, 4.5, NULL, 0.10),
-(2, 'Beslagskruer 4,0x50mm 250stk', '3 pakke til montering af universalbeslag + hulbånd', 'pakke', 50, 4, NULL, 0.08),
-(2, 'Bræddebolt 10x120mm', '18 stk til montering af rem på stolper', 'stk', 120, 10, NULL, 0.75),
-(2, 'Firkantskiver 40x40x11mm', '12 stk til montering af rem på stolper', 'stk', 40, 40, 11, 0.25),
-(2, 'Skruer 4,5x70mm 400stk', '2 pakke til montering af yderste beklædning', 'pakke', 70, 4.5, NULL, 0.12),
-(2, 'Skruer 4,5x50mm 300stk', '2 pakke til montering af inderste beklædning', 'pakke', 50, 4.5, NULL, 0.10),
-(2, 'Stalddørsgreb 50x75', '1 sæt til lås på dør i skur', 'sæt', 50, 75, NULL, 5.00),
-(2, 'Hængsel 390mm', '2 stk til skurdør', 'stk', 390, NULL, NULL, 3.50),
-(2, 'Vinkelbeslag 35', '32 stk til montering af løsholter i skur', 'stk', 35, NULL, NULL, 0.20);
+
+-- Metal
+(2, 'Metalramme 2×3 m', 'Støttestruktur til skur 2x3 m', 'stk', 250, 5, 5, 300),
+(2, 'Metalramme 3×4 m', 'Støttestruktur til skur 3x4 m', 'stk', 300, 5, 5, 400),
+
+-- Plast/Polycarbonat
+(3, 'Plastmo Ecolite 360', 'Blåtonet tagplade, monteres på spær', 'stk', 360, NULL, NULL, 35.00),
+(3, 'Plastmo Ecolite 420', 'Blåtonet tagplade, monteres på spær', 'stk', 420, NULL, NULL, 40.00),
+(3, 'Plastmo Ecolite 600', 'Blåtonet tagplade, monteres på spær', 'stk', 600, NULL, NULL, 50.00),
+
+-- Tagmateriale
+(4, 'Tagpap 2×3 m', 'Til fladt eller let skrånende tag', 'm2', 200, 100, 0, 50.00),
+(4, 'Tagpap 3×4 m', 'Til fladt eller let skrånende tag', 'm2', 300, 150, 0, 80.00),
+(4, 'Bølgeplast 2×3 m', 'Let tagmateriale', 'm2', 200, 100, 0, 70.00),
+(4, 'Bølgeplast 3×4 m', 'Let tagmateriale', 'm2', 300, 150, 0, 100.00),
+
+-- Gulv
+(5, 'Træplade gulv 2×3 m', 'Valgfrit gulv til skur 2x3 m', 'stk', 200, 100, 2, 80.00),
+(5, 'Træplade gulv 3×4 m', 'Valgfrit gulv til skur 3x4 m', 'stk', 300, 150, 2, 120.00),
+
+-- Vinduer
+(6, 'Polycarbonat-vindue 60x60 cm', 'Gennemsigtigt vindue', 'stk', 60, 60, 2, 120.00),
+(6, 'Polycarbonat-vindue 80x80 cm', 'Gennemsigtigt vindue', 'stk', 80, 80, 2, 180.00),
+
+-- Døre
+(7, 'Trædør 90x200 cm', 'Standard skurdør', 'stk', 90, 200, 3, 250.00),
+(7, 'Trædør 100x210 cm', 'Større skurdør', 'stk', 100, 210, 3, 300.00),
+
+-- Skruer
+(8, 'Skruer 4,5x50mm', 'Til inderste beklædning', 'pakke', 50, 4.5, NULL, 0.10),
+(8, 'Skruer 4,5x70mm', 'Til yderste beklædning', 'pakke', 70, 4.5, NULL, 0.12),
+(8, 'Skruer 5x50mm', 'Til tagplader', 'pakke', 50, 5, NULL, 0.50),
+(8, 'Universal skruer 190mm højre', 'Til montering af spær på rem', 'stk', 190, NULL, NULL, 1.50),
+(8, 'Universal skruer 190mm venstre', 'Til montering af spær på rem', 'stk', 190, NULL, NULL, 1.50),
+
+-- Beslag
+(9, 'Vinkelbeslag 35', 'Til montering af løsholter', 'stk', 35, NULL, NULL, 0.20),
+(9, 'Hjørnebeslag', 'Forstærker hjørner i skurrammen', 'stk', 10, 10, 0.3, 20.00),
+(9, 'Hængsel 390mm', 'Til skurdør', 'stk', 390, NULL, NULL, 3.50),
+(9, 'Stalddørsgreb 50x75', 'Til lås på skurdør', 'sæt', 50, 75, NULL, 5.00);
 
 INSERT INTO carport_category (name) VALUES
 ('Fladt tag'),
