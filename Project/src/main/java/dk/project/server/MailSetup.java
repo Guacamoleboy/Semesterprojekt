6a8b3f1b121490660518a714aaf1bd5b593dd69e
Guacamoleboy <cph-jl325@stud.ek.dk>
2025-12-13 14:08:23 +0100

    Deployment 13/11-2025 | 14:08

--- Project/src/main/java/dk/project/server/MailSetup.java
+++ Project/src/main/java/dk/project/server/MailSetup.java
@@ -1,3 +1,18 @@
+/*
+
+    Switched to MailSetupAPI since Digital Ocean blocks default ports for SMTP.
+    See MailSetupAPI for config.
+
+    ______________________________________________________
+
+    DO NOT REMOVE THIS CLASS
+
+    ______________________________________________________
+
+    - Guac
+
+*/
+
 // Package
 package dk.project.server;
 
@@ -10,10 +25,10 @@ import java.util.Properties;
 public class MailSetup {
 
     // Attributes
-    private static final String SMTP_HOST = "send.one.com";
-    private static final int SMTP_PORT = 465;
+    private static final String SMTP_HOST = "smtp-relay.brevo.com";
+    private static final String SMTP_PORT = "587";
     private static final String OUR_EMAIL = "fog@travlr.dk";
-    private static final String OUR_PASSWORD = "Fogpassword123!";
+    private static final String OUR_PASSWORD = "XX";
 
     // _________________________________________________
 
@@ -22,10 +37,12 @@ public class MailSetup {
         Properties props = new Properties();
         props.put("mail.smtp.auth", "true");
         props.put("mail.smtp.host", SMTP_HOST);
-        props.put("mail.smtp.port", String.valueOf(SMTP_PORT));
-        props.put("mail.smtp.ssl.enable", "true"); // Important
-        props.put("mail.smtp.socketFactory.port", "465"); // Issue #223 fix
-        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory"); // Issue #223 fix
+        props.put("mail.smtp.port", SMTP_PORT);
+        props.put("mail.smtp.starttls.enable", "true");
+        props.put("mail.smtp.ssl.enable", "false");
+        props.put("mail.smtp.connectiontimeout", "10000"); // Timeout Fix
+        props.put("mail.smtp.timeout", "10000"); // Timeout Fix
+        props.put("mail.smtp.writetimeout", "10000"); // Timeout Fix
         props.put("mail.debug", "true");
 
         return Session.getInstance(props, new Authenticator() {
